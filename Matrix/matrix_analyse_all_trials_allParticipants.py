0e882ea0627a86ce0317ad0214e2c9b2438fc5e3
Leny-Vinceslas <leny.vinceslas@gamil.com>
2024-04-10 18:15:12 +0100

    xml parsed

---
+++ Matrix/matrix_analyse_all_trials_allParticipants.py
@@ -0,0 +1,66 @@
+
+#%% Load Modules and definitions
+import parseXML
+import os
+import numpy as np
+import numpy as np 
+import os
+import scipy.io as spio
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+from importlib import reload 
+
+
+
+#%%
+# Find all OAE files ---------------------------------------------
+# directory_path = os.getcwd(../)+'\\'
+# directory_path = 'c:\\Users\\lenyv\\OneDrive - University College London\\UCL\\Hyperacusis-project\\Python\\Loudness Analysis Scripts\\Participants\\'
+directory_path =os.path.dirname(os.getcwd())+"\\Participants\\"
+extension = ".xml"
+name_pattern =r'(Matrix)_(\d{4})\.xml$'  
+matched_files = []
+IDs=[]
+
+for root, _, files in os.walk(directory_path):
+    
+    for file in files:
+        if file.endswith(extension) and re.match(name_pattern, file):
+            matched_files.append(os.path.join(root, file))
+
+ID_pattern = r'\\\w{6}_(\d{4})\.xml$'
+
+# List to store extracted IDs
+
+# Iterate over file paths
+for matched_file in matched_files:
+    # Search for pattern in each file path
+    match = re.search(ID_pattern, matched_file)
+    if match:
+        ID = match.group(1)
+        if ID not in IDs:
+            IDs.append(ID)
+
+participantIDs=IDs
+# Print the extracted IDs
+print("IDs in the list:")
+print(IDs)
+#%% 
+# Parse XML and import blocks obj-------------
+reload(parseXML)
+blockFromCls = parseXML.Matrix2dic()
+CLSs=[]
+
+for ID in participantIDs:
+
+    blocks=[]
+    CLSs.append({})
+    CLSs[-1]['ID']=ID
+
+    for file in matched_files:
+        if re.search(rf'\w+_{ID}\.xml$' , os.path.basename(file)):
+            matrix, blocks=blockFromCls.parse(file)
+            CLSs[-1]=matrix  
+            print("-----> xml data parsed:", str(file))
+
--- Matrix/parseXML.py
+++ Matrix/parseXML.py
@@ -3,10 +3,11 @@ import numpy as np
 from itertools import islice
 
 
-class Cls2dic:
+class Matrix2dic:
     def __init__(self):
         self.path = []
         self.blocks=[]
+        self.matrix={}
         
  
     def parse(self, path):
@@ -21,74 +22,39 @@ class Cls2dic:
         root = tree.getroot()
 
         for elem in root :
-            # print(elem.tag, elem.attrib)
-            
+            print(elem.tag, elem.attrib)
+            if elem.tag == 'Value': self.matrix[elem.attrib['Name']]=elem.attrib['Value']
             for child1 in elem:
-                # print('-->', child1.tag, child1.attrib)
+                print('-->', child1.tag, child1.attrib)
+                if child1.tag == 'Value': self.matrix[child1.attrib['Name']]=child1.attrib['Value']
                 for child12 in child1:
                     for child2 in islice(child12.iter(), 1, None):
-                        # print('---->', child2.tag, child2.attrib)
-                        # for child3 in child2:
-                        #     print('------>', child3.tag, child3.attrib)
-                        #     if child3.tag == "Value" and child3.attrib['Name']== "ChannelLevels"  :
-                                # print('======CHILD3', child3.attrib['Name'],child3.attrib['Value'])
-                                
-                        if child2.tag == "Value" and child2.attrib['Name']== "GroupId"  : 
-                            # print('------ New block -------------->')
-                            self.blocks.append({})      
-                            
-                        if child2.tag == "Value" and child2.attrib['Name']== "ChannelLevels"  :
-                            
-                            if child2.attrib['Value']=='- 0':
-                                 self.blocks[-1]['Side']=1
-                                 self.blocks[-1]['SideStr']='Left'
-                            elif child2.attrib['Value']=='0':
-                                 self.blocks[-1]['Side']=0
-                                 self.blocks[-1]['SideStr']='Right'
-                            elif child2.attrib['Value']=='0 0':
-                                 self.blocks[-1]['Side']=00
-                                 self.blocks[-1]['SideStr']='diotic'
-                                
-                        if child2.tag=='Subheader' and child2.attrib['Name']=='Data':
+                        print('---->', child2.tag, child2.attrib)   
+                        if child2.tag == 'Subheader' and child2.attrib['Name']== "Blocks" and child2.attrib['Comment']== "" :
+                            print('------ enter data -------------->')
                             for child3 in child2:
-                                data_fit_level.append(float(child3.get('Value').split(' ')[0]))
-                                data_fit_CU.append(float(child3.get('Value').split(' ')[1]))
-                            
-                            self.blocks[-1]['Data_fit_level']=(np.asarray(data_fit_level))
-                            self.blocks[-1]['Data_fit_CU']=(np.asarray(data_fit_CU))    
-                            
-                        elif child2.tag=='Value' and not child2.attrib['Name']=='':
-                            self.blocks[-1][child2.attrib['Name']]=child2.attrib['Value']
-                                
-                            # for child4 in child3:
-                            #     print('-------->', child4.tag, child4.attrib)
-                                
-                                
-                            #     for child5 in child4:
-                            #         print('---------->', child5.tag, child5.attrib)
-                            #         for child5 in child4:
-                            #             print('+----------->', child5.tag, child5.attrib)
-                            #             # for child6 in child5:
-                            #             for child6 in islice(child5.iter(), 1, None):
-                            #                 print('X------------->', child6.tag, child6.attrib)
-                            #                 # for child6 in child5:
-                            #                 #     print('------------>', child6.tag, child6.attrib)
-                            #                 #     for child7 in child5:
-                            #                 #         for child8 in child7:
-                            #                 #             print('-------------->', child8.tag, child8.attrib)
-                                    
-                        if child2.tag == "Value" and child2.attrib['Name']== "CU":
-                                                CU_list.append(child2.get('Value'))
-                        if child2.tag == "Value" and child2.attrib['Name']== "Level":
-                                                level_list.append(child2.get('Value'))
-                    if level_list and CU_list:  
-                        self.blocks[-1]['CU'] = np.asanyarray([float(i) for i in CU_list])
-                        self.blocks[-1]['Level'] = np.asanyarray([float(i) for i in level_list])
-                        level_list=[]
-                        CU_list=[]
-                        data_fit_level=[]
-                        data_fit_CU=[]
-        return self.blocks
+                                   if child3.tag == 'Subheader' and child3.attrib['Name']== "" and child3.attrib['Comment']== "" :
+                                    for child4 in child3:
+                                        print('1------->', child4.tag, child4.attrib)
+                                        if child4.tag == 'Value': self.matrix[child4.attrib['Name']]=child4.attrib['Value']
+                                        if child4.tag == 'Subheader' and child4.attrib['Name']== "Trials" and child4.attrib['Comment']== "" :                             
+                                            print('----- New block -------------->')
+                                            self.blocks.append({}) 
+                                            block=[]
+                                            data_sentances=[]
+                                            for child5 in child4:
+                                                # print('2-------->', child5.tag, child5.attrib)
+                                                block.append({}) 
+                                                if child5.tag == 'Subheader' and child5.attrib['Name']== "" and child5.attrib['Comment']== "" :
+                                                    # print('----- New sentense-------------->')
+                                                    data_sentances.append({})
+                                                    for child6 in child5:
+                                                        # print('4--------->', child6.tag, child6.attrib)
+                                                        data_sentances[-1][child6.attrib['Name']]=child6.attrib['Value']
+                                            self.blocks[-1]=data_sentances
+                    self.matrix['Blocks']=self.blocks                   
+
+        return self.matrix, self.blocks
                         
  
  
